95fa35ecb17c6578e1109bc5155bb7b46271869a
ashish-witarist <ashish@witarist.com>
2025-04-09 17:47:57 +0530

    added lead form

--- src/sections/home/Banner.tsx
+++ src/sections/home/Banner.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 import dynamic from 'next/dynamic';
+import FormComponent from '@/components/FormComponent';
 
 const Carousel = dynamic(() => import('nuka-carousel'), { ssr: false });
 
@@ -16,6 +17,7 @@ const bannerData = [
     },
   },
 ];
+
 const BannerComponent: React.FC = () => {
   const defaultControlsConfig = {
     pagingDotsStyle: {
@@ -24,50 +26,57 @@ const BannerComponent: React.FC = () => {
   };
 
   return (
-    <div className=''>
-      <Carousel
-        autoplay
-        autoplayInterval={5000}
-        className='border-0 shadow-2xl drop-shadow-2xl'
-        defaultControlsConfig={defaultControlsConfig}
-        wrapAround
-        dragging
-        enableKeyboardControls
-        pauseOnHover
-        renderCenterLeftControls={({ previousSlide }) => (
-          <button
-            onClick={previousSlide}
-            className='ml-3 hidden h-11 w-11 items-center justify-center rounded-full bg-brandPurpleDark bg-opacity-70 text-4xl text-white transition duration-300 ease-in-out hover:bg-opacity-100 md:flex'
-          >
-            <HiChevronLeft className='mr-1' />
-          </button>
-        )}
-        renderCenterRightControls={({ nextSlide }) => (
-          <button
-            onClick={nextSlide}
-            className='mr-3 hidden h-11 w-11 items-center justify-center rounded-full bg-brandPurpleDark bg-opacity-70 text-4xl text-white transition duration-300 ease-in-out hover:bg-opacity-100 md:flex'
-          >
-            <HiChevronRight className='ml-1' />
-          </button>
-        )}
-      >
-        {bannerData.length > 0 ? (
-          bannerData.map((banner) => (
-            <Link href={banner.url || '#'} target='_blank' rel='noreferrer' key={banner.id}>
-              <Image
-                src={banner.image.url}
-                alt={banner.title}
-                width={1920}
-                height={1080}
-                className='h-full w-full object-cover'
-                priority
-              />
-            </Link>
-          ))
-        ) : (
-          <div>No banners available</div>
-        )}
-      </Carousel>
+    <div className='flex flex-col gap-y-6 pb-5 md:flex-row md:pb-8'>
+      {/* Banner Section */}
+      <div className='h-full w-full md:w-2/3'>
+        <Carousel
+          autoplay
+          autoplayInterval={5000}
+          className='border-0 shadow-2xl drop-shadow-2xl'
+          defaultControlsConfig={defaultControlsConfig}
+          wrapAround
+          dragging
+          enableKeyboardControls
+          pauseOnHover
+          renderCenterLeftControls={({ previousSlide }) => (
+            <button
+              onClick={previousSlide}
+              className='ml-3 hidden h-11 w-11 items-center justify-center rounded-full bg-brandPurpleDark bg-opacity-70 text-4xl text-white transition duration-300 ease-in-out hover:bg-opacity-100 md:flex'
+            >
+              <HiChevronLeft className='mr-1' />
+            </button>
+          )}
+          renderCenterRightControls={({ nextSlide }) => (
+            <button
+              onClick={nextSlide}
+              className='mr-3 hidden h-11 w-11 items-center justify-center rounded-full bg-brandPurpleDark bg-opacity-70 text-4xl text-white transition duration-300 ease-in-out hover:bg-opacity-100 md:flex'
+            >
+              <HiChevronRight className='ml-1' />
+            </button>
+          )}
+        >
+          {bannerData.length > 0 ? (
+            bannerData.map((banner) => (
+              <Link href={banner.url || '#'} target='_blank' rel='noreferrer' key={banner.id}>
+                <Image
+                  src={banner.image.url}
+                  alt={banner.title}
+                  width={1920}
+                  height={1080}
+                  className='h-full w-full object-cover'
+                  priority
+                />
+              </Link>
+            ))
+          ) : (
+            <div>No banners available</div>
+          )}
+        </Carousel>
+      </div>
+      {/* Form Section */}
+      <div className='flex items-center justify-center bg-[#005e7e] shadow-lg md:w-1/3'>
+        <FormComponent title={'Book your Appointment'} />
+      </div>
     </div>
   );
 };
