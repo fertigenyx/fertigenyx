97d7504466e8ff7e4f53f886b3f5a8e3697be711
ashish-witarist <ashish@witarist.com>
2025-04-24 13:10:58 +0530

    add gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,12 +22,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5MMZWXWK');</script>
 <!-- End Google Tag Manager -->
-
             `,
           }}
         />
       </Head>
       <body className='antialiased'>
+        <Main />
+        <NextScript />
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
@@ -38,8 +39,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             `,
           }}
         />
-        <Main />
-        <NextScript />
       </body>
     </Html>
   );
