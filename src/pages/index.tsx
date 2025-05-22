9dafae9f57d9027d441f252c9cbe56b063e1c12b
ashish-witarist <ashish@witarist.com>
2025-05-22 17:01:22 +0530

    integrate zoho form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,6 +49,7 @@ export default function Home() {
         <IvfIndications ref={sectionRefs['ivf-and-its-indications']} />
         <Faq />
         <Cta />
+        <iframe src='./ZohoForm.html' width='100%' height={300} />
         <RelatedSearches />
       </main>
       <Footer />
