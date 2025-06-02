dc228f6b725336acd1d71e1fc7e507da1a50d715
ashish-witarist <ashish@witarist.com>
2025-06-02 13:28:48 +0530

    generate sitemap automatically

---
+++ next-sitemap.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.fertigenyx.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  exclude: ['/404', '/500'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+};
